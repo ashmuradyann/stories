d71ffd24cd0ff5c7c4ca21669298ba4dffc1dbde
ashmuradyann <ashot.muradyan16@gmail.com>
2022-07-25 11:43:11 +0300

    fixed forms validation

--- src/components/horizontal/form/Form.jsx
+++ src/components/horizontal/form/Form.jsx
@@ -8,9 +8,25 @@ const Form = ({ close, setSubmited, setShowForm }) => {
     const [state, setState] = useState("form")
     const [name, setName] = useState("")
     const [number, setNumber] = useState("")
-    const [phoneNumberWarnMessage, setPhoneNumberWarnMessage] = useState(null)
+    const [warnMessage, setWarnMessage] = useState(null)
 
     const submit = () => {
+        if (number.length < 12 && name === "") {
+            setWarnMessage("Заполните пожалуйста все поля")
+            return
+        }
+        if (name === "") {
+            setWarnMessage("Напишите пожалуйста верное имя")
+            return
+        }
+        if (number === "") {
+            setWarnMessage("Напишите пожалуйста номер телефона")
+            return
+        }
+        if (number.length < 12) {
+            setWarnMessage("Некорректный номер телефона")
+            return
+        }
         console.log("ИМЯ: " + name, "НОМЕР: " + number)
         setState("finish")
         setTimeout(() => {
@@ -24,12 +40,6 @@ const Form = ({ close, setSubmited, setShowForm }) => {
         if (event.target.value.length !== 13) {
             setNumber("+7" + event.target.value.replaceAll("+7", "").replace(/[^+\d]/g, ''))
         }
-        setTimeout(() => {
-            if (event.target.value.length < 12) {
-                setPhoneNumberWarnMessage(true)
-            }
-        }, 1000)
-        setPhoneNumberWarnMessage(false)
     }
 
     return (
@@ -43,14 +53,16 @@ const Form = ({ close, setSubmited, setShowForm }) => {
                         <h3>МЫ СВЯЖЕМСЯ С ВАМИ</h3>
                         <div className="group">
                             <input value={name} className="form-input" type="text" onChange={(e) => setName(e.target.value)} />
-                            <label className={`${name.length ? "shrink" : ""} form-input-label`}>ВАШЕ ИМЯ</label>
+                            <label className={`${name.length ? "shrink" : ""} form-input-label`}>ИМЯ</label>
                         </div>
                         <div className="group">
                             <input value={number !== "+7" ? number : ""} className="form-input" type="text" onChange={phoneNumberHandleChange} />
-                            <label className={`${number.length ? "shrink" : ""} form-input-label`}>ВАШ ТЕЛЕФОН</label>
+                            <label className={`${number.length ? "shrink" : ""} form-input-label`}>НОМЕР ТЕЛЕФОНА</label>
                         </div>
-                        {phoneNumberWarnMessage && <p>Неверный номер телефона</p>}
-                        <button onClick={submit} disabled={name === "" || number.length !== 12}>ОТПРАВИТЬ</button>
+                        <div>
+                            <p>{warnMessage}</p>
+                        </div>
+                        <button onClick={submit}>ОТПРАВИТЬ</button>
                     </div>
                     : <div className="finish__container">
                         <h2>ЗАЯВКА ОТПРАВЛЕНА!</h2>
