9702968280fed19ed201e402610031295ecda31c
ashmuradyann <ashot.muradyan16@gmail.com>
2022-07-26 19:43:54 +0300

    added working qr code

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -1,15 +1,16 @@
-import { ReactComponent as QrCode } from './QrCode.svg'
-
 import './information.scss'
 
 const Information = () => {
+
+    const url = "https://ashmuradyann.github.io/stories/"
+
     return (
         <div className="info__wrapper">
             <div className="info__container">
                 <h1>Простая партнёрская программа для бизнеса</h1>
                 <h5>Привлекайте новых клиентов без вложений в рекламу!</h5>
                 <div className="qr__block">
-                    <QrCode />
+                    <img src={`http://api.qrserver.com/v1/create-qr-code/?data=${url}&size=200x200`} alt="QrCode" />
                     <div>
                         <p>Наведите камеру на QR-код</p>
                         <p>и откройте этот стори-лендинг на своём смартфоне.</p>
