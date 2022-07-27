32dfaf9629e02d4ebccf7ee168c14a1723e47449
ashmuradyann <ashot.muradyan16@gmail.com>
2022-07-27 17:56:21 +0300

    now needed config file to run

--- src/components/horizontal/Horizontal.jsx
+++ src/components/horizontal/Horizontal.jsx
@@ -12,14 +12,7 @@ import image4 from '../../assets/images/image4.jpg'
 import image5 from '../../assets/images/image5.jpg'
 import image6 from '../../assets/images/image6.jpg'
 
-import { ReactComponent as Facebook } from './icons/facebook.svg'
-import { ReactComponent as Instagram } from './icons/instagram.svg'
-import { ReactComponent as Whatsapp } from './icons/whatsapp.svg'
-import { ReactComponent as Viber } from './icons/viber.svg'
-import { ReactComponent as Telegram } from './icons/telegram.svg'
-
 import './horizontal.scss'
-
 
 const Horizontal = ({ submited, setSubmited }) => {
 
@@ -34,108 +27,380 @@ const Horizontal = ({ submited, setSubmited }) => {
 
   const lower410 = window.matchMedia('(max-width: 410px)')
 
-  const [popup, setPopup] = useState(null)
+  const [popup, setPopup] = useState({
+    bool: false,
+    data: []
+  })
+
+  // const [popup, setPopup] = useState(null)
   const [firstPlay, setFirstPlay] = useState(null)
-  const [currentIndex, setCurrentIndex] = useState(null)
 
   useEffect(() => {
     setFirstPlay(true)
   }, [])
 
-  const images = [
+  const data = [
     {
       url: image1,
-      seeMore: () => null,
-      seeMoreCollapsed: ({ action }) => {
-        if (popup || firstPlay) {
-          action("pause")
-        } else {
-          action("")
+      duration: 15000,
+      paused: false,
+      popup: {
+        needed: true,
+        data: {
+          button: {
+            text: "Подробнее",
+            backgroundColor: "#333333",
+            iconUrl: "https://img.icons8.com/ios-glyphs/30/ffffff/facebook-new.svg"
+          },
+          h1: "Заголовок",
+          text: [{
+            h2: "Заголовокh2",
+            p: "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia"
+          },
+          {
+            h2: "Заголовокh2",
+            p: "Contrary to popular belief, Lorem Ipsum is not simply random text."
+          },
+          {
+            h2: "Заголовокh2",
+            p: "from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,comes from a line in section 1.10.32."
+          }],
         }
-        setCurrentIndex(false)
-        return (
-          <div style={popup ? { opacity: 0, transition: "0.3s" } : null} className="seeMore__wrapper">
-            <button style={{}} className="seeMoreCollapsed" onClick={() => setPopup(true)}>
-              Подробнее
-            </button>
-          </div>
-        )
+      },
+      buttons: {
+        needed: false,
+      },
+      form: {
+        needed: false
       }
     },
-    image2,
+    {
+      url: image2,
+      duration: 5000,
+      paused: false,
+      popup: {
+        needed: false
+      },
+      buttons: {
+        needed: false
+      },
+      form: {
+        needed: false
+      }
+    },
     {
       url: image3,
-      seeMore: () => null,
-      duration: 15000,
-      seeMoreCollapsed: ({ action }) => {
-        return (
-          <div style={{ flexDirection: 'column' }} className="seeMore__wrapper">
-            <button className="seeMoreCollapsed otherButton fb">
-              <Facebook /> Мы в Facebook
-            </button>
-            <button className="seeMoreCollapsed otherButton inst">
-              <Instagram /> Мы в Instagram
-            </button>
-          </div>
-        )
+      duration: 5000,
+      paused: false,
+      popup: {
+        needed: false
+      },
+      buttons: {
+        needed: true,
+        buttonData: [
+          {
+            text: "Мы в Facebook",
+            backgroundColor: "#1673ea",
+            iconUrl: "https://img.icons8.com/ios-glyphs/30/ffffff/facebook-new.svg"
+          },
+          {
+            text: "Мы в Instagram",
+            backgroundColor: "#d83364",
+            iconUrl: "https://img.icons8.com/material-outlined/24/ffffff/instagram-new--v1.svg"
+          },
+        ]
+      },
+      form: {
+        needed: false
       }
     },
     {
       url: image4,
-      seeMore: () => null,
-      duration: 15000,
-      seeMoreCollapsed: () => (
-        <div style={{ flexDirection: "column", paddingBottom: "20px" }} className="seeMore__wrapper">
-          <button className="seeMoreCollapsed otherButton2 wp">
-            <Whatsapp /> наш WhatsApp
-          </button>
-          <button className="seeMoreCollapsed otherButton2 tg">
-            <Telegram /> наш Telegram
-          </button>
-          <button className="seeMoreCollapsed otherButton2 vb">
-            <Viber /> наш Viber
-          </button>
-        </div>
-      )
+      duration: 5000,
+      paused: false,
+      popup: {
+        needed: false
+      },
+      buttons: {
+        needed: true,
+        buttonData: [
+          {
+            text: "наш WhatsApp",
+            backgroundColor: "#48c857",
+            iconUrl: "https://img.icons8.com/ios-glyphs/30/ffffff/facebook-new.svg"
+          },
+          {
+            text: "наш Telegram",
+            backgroundColor: "#27a3e1",
+            iconUrl: "https://img.icons8.com/ios-glyphs/30/ffffff/telegram-app.svg"
+          },
+          {
+            text: "наш Viber",
+            backgroundColor: "#7d539f",
+            iconUrl: "https://img.icons8.com/ios-glyphs/30/ffffff/viber.png"
+          },
+        ]
+      },
+      form: {
+        needed: false
+      }
     },
-    image5,
+    {
+      url: image5,
+      duration: 5000,
+      paused: false,
+      popup: {
+        needed: false
+      },
+      buttons: {
+        needed: false,
+      },
+      form: {
+        needed: false
+      }
+    },
     {
       url: image6,
-      duration: 15000,
-      seeMore: ({ close }) => <Form close={close} setSubmited={setSubmited} />,
-      seeMoreCollapsed: ({ toggleMore, action }) => {
-        action("pause")
-        return (
-          <div className="seeMore__wrapper">
-            {<button style={submited && { pointerEvents: "none", backgroundColor: "#909090" }} className="seeMoreCollapsed lastBtn" onClick={() => toggleMore(true)}>
-              {!submited ? "Отправьте Заявку" : "Заявка Отправлена"}
-            </button>}
-            <button className="seeMoreCollapsed lastBtn" onClick={() => setCurrentIndex(true)}>
-              Самое Начало
-            </button>
-          </div>
-        )
+      duration: 10000,
+      paused: true,
+      popup: {
+        needed: false
+      },
+      buttons: {
+        needed: false,
+      },
+      form: {
+        needed: true,
+        toggleButton: {
+          backgroundColor: "#333333"
+        }
       }
     }
   ]
+
+  const readyToRend = data.map((slide, i) => {
+    return {
+      url: slide.url,
+      duration: 5000,
+      seeMore: slide.form.needed ? ({ close }) => <Form close={close} setSubmited={setSubmited} /> : slide.popup ? () => null : null,
+      seeMoreCollapsed: ({ toggleMore, action }) => {
+        if (popup.bool || firstPlay || slide.paused) {
+          action("pause")
+        } else {
+          action("")
+        }
+        if (slide.popup.needed) {
+          return (
+            <div key={i} style={popup.bool ? { opacity: 0, transition: "0.3s" } : null} className="seeMore__wrapper">
+              <button style={{ backgroundColor: slide.popup.data.button.backgroundColor }} className="seeMoreCollapsed popup" onClick={() => setPopup({ ...popup, bool: true, data: slide.popup.data })}>
+                {slide.popup.data.button.text}
+              </button>
+            </div>
+          )
+        }
+        if (slide.buttons.needed) {
+          return (
+            <div className="seeMore__wrapper">
+              {slide.buttons.buttonData.map(({ text, backgroundColor, iconUrl }, i) => {
+                return <button style={{ backgroundColor: backgroundColor }} className="seeMoreCollapsed otherButton">
+                  <img src={iconUrl} alt={i} /> {text}
+                </button>
+              })}
+            </div>
+          )
+        }
+        if (slide.form.needed) {
+          return (
+            <div className="seeMore__wrapper">
+              {<button style={submited ? { pointerEvents: "none", backgroundColor: "#808080" } : { backgroundColor: slide.form.toggleButton.backgroundColor }} className="seeMoreCollapsed" onClick={() => toggleMore(true)}>
+                {!submited ? "Отправьте Заявку" : "Заявка Отправлена"}
+              </button>}
+            </div>
+          )
+        }
+      }
+    }
+  })
 
   return (
     <div className="container" onClick={() => setFirstPlay(false)}>
       <div className="wrapper">
         <Stories
-          currentIndex={currentIndex ? 0 : null}
-          stories={images}
+          stories={readyToRend}
           width={lower410.matches ? "100vw" : "400px"}
           height={lower410.matches ? "100%" : "700px"}
           defaultInterval={5000}
         />
-        {popup ? <Popup setPopup={setPopup} /> : null}
+        {popup.bool ? <Popup popup={popup} setPopup={setPopup} /> : null}
       </div>
-      <div style={{display: "none"}}>
+      <div style={{ display: "none" }}>
         {imagesArr.map(imgSrc => <img src={imgSrc} alt="img" />)}
       </div>
     </div>
   )
+
+  // const images = [
+  //   {
+  //     url: image1,
+  //     seeMore: () => null,
+  //     seeMoreCollapsed: ({ action }) => {
+  //       if (popup.bool || firstPlay) {
+  //         action("pause")
+  //       } else {
+  //         action("")
+  //       }
+  //       return (
+  //         <div style={popup.bool ? { opacity: 0, transition: "0.3s" } : null} className="seeMore__wrapper">
+  //           <button style={{}} className="seeMoreCollapsed" onClick={() => setPopup({ bool: true, data: "{popup ? <Popup setPopup={setPopup} /> : null}" })}>
+  //             Подробнее
+  //           </button>
+  //         </div>
+  //       )
+  //     }
+  //   },
+  //   image2,
+  //   {
+  //     url: image3,
+  //     seeMore: () => null,
+  //     duration: 15000,
+  //     seeMoreCollapsed: ({ action }) => {
+  //       return (
+  //         <div className="seeMore__wrapper">
+  //           <button className="seeMoreCollapsed otherButton fb">
+  //             <Facebook /> Мы в Facebook
+  //           </button>
+  //           <button className="seeMoreCollapsed otherButton inst">
+  //             <Instagram /> Мы в Instagram
+  //           </button>
+  //         </div>
+  //       )
+  //     }
+  //   },
+  //   {
+  //     url: image4,
+  //     seeMore: () => null,
+  //     duration: 15000,
+  //     seeMoreCollapsed: () => (
+  //       <div className="seeMore__wrapper">
+  //         <button className="seeMoreCollapsed otherButton2 wp">
+  //           <Whatsapp /> наш WhatsApp
+  //         </button>
+  //         <button className="seeMoreCollapsed otherButton2 tg">
+  //           <Telegram /> наш Telegram
+  //         </button>
+  //         <button className="seeMoreCollapsed otherButton2 vb">
+  //           <Viber /> наш Viber
+  //         </button>
+  //       </div>
+  //     )
+  //   },
+  //   image5,
+  //   {
+  //     url: image6,
+  //     duration: 15000,
+  //     seeMore: ({ close }) => <Form close={close} setSubmited={setSubmited} />,
+  //     seeMoreCollapsed: ({ toggleMore, action }) => {
+  //       action("pause")
+  //       return (
+  //         <div className="seeMore__wrapper">
+  //           {<button style={submited && { pointerEvents: "none", backgroundColor: "#909090" }} className="seeMoreCollapsed lastBtn" onClick={() => toggleMore(true)}>
+  //             {!submited ? "Отправьте Заявку" : "Заявка Отправлена"}
+  //           </button>}
+  //         </div>
+  //       )
+  //     }
+  //   }
+  // ]
+
+  // const images = [
+  //   {
+  //     url: image1,
+  //     seeMore: () => null,
+  //     seeMoreCollapsed: ({ action }) => {
+  //       if (popup || firstPlay) {
+  //         action("pause")
+  //       } else {
+  //         action("")
+  //       }
+  //       return (
+  //         <div style={popup ? { opacity: 0, transition: "0.3s" } : null} className="seeMore__wrapper">
+  //           <button style={{}} className="seeMoreCollapsed" onClick={() => setPopup(true)}>
+  //             Подробнее
+  //           </button>
+  //         </div>
+  //       )
+  //     }
+  //   },
+  //   image2,
+  //   {
+  //     url: image3,
+  //     seeMore: () => null,
+  //     duration: 15000,
+  //     seeMoreCollapsed: ({ action }) => {
+  //       return (
+  //         <div className="seeMore__wrapper">
+  //           <button className="seeMoreCollapsed otherButton fb">
+  //             <Facebook /> Мы в Facebook
+  //           </button>
+  //           <button className="seeMoreCollapsed otherButton inst">
+  //             <Instagram /> Мы в Instagram
+  //           </button>
+  //         </div>
+  //       )
+  //     }
+  //   },
+  //   {
+  //     url: image4,
+  //     seeMore: () => null,
+  //     duration: 15000,
+  //     seeMoreCollapsed: () => (
+  //       <div className="seeMore__wrapper">
+  //         <button className="seeMoreCollapsed otherButton2 wp">
+  //           <Whatsapp /> наш WhatsApp
+  //         </button>
+  //         <button className="seeMoreCollapsed otherButton2 tg">
+  //           <Telegram /> наш Telegram
+  //         </button>
+  //         <button className="seeMoreCollapsed otherButton2 vb">
+  //           <Viber /> наш Viber
+  //         </button>
+  //       </div>
+  //     )
+  //   },
+  //   image5,
+  //   {
+  //     url: image6,
+  //     duration: 15000,
+  //     seeMore: ({ close }) => <Form close={close} setSubmited={setSubmited} />,
+  //     seeMoreCollapsed: ({ toggleMore, action }) => {
+  //       action("pause")
+  //       return (
+  //         <div className="seeMore__wrapper">
+  //           {<button style={submited && { pointerEvents: "none", backgroundColor: "#909090" }} className="seeMoreCollapsed lastBtn" onClick={() => toggleMore(true)}>
+  //             {!submited ? "Отправьте Заявку" : "Заявка Отправлена"}
+  //           </button>}
+  //         </div>
+  //       )
+  //     }
+  //   }
+  // ]
+
+  // return (
+  //   <div className="container" onClick={() => setFirstPlay(false)}>
+  //     <div className="wrapper">
+  //       <Stories
+  //         stories={images}
+  //         width={lower410.matches ? "100vw" : "400px"}
+  //         height={lower410.matches ? "100%" : "700px"}
+  //         defaultInterval={5000}
+  //       />
+  //       {popup ? <Popup setPopup={setPopup} /> : null}
+  //     </div>
+  //     <div style={{display: "none"}}>
+  //       {imagesArr.map(imgSrc => <img src={imgSrc} alt="img" />)}
+  //     </div>
+  //   </div>
+  // )
 }
 
 export default memo(Horizontal)
